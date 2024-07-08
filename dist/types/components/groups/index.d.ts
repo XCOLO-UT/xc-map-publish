b1e47c8b2ebfa61ed06ebbc1ce70687c683706a6
DKJung <zzliou33@xcolo.co.kr>
2024-07-08 17:42:27 +0900

    오타수정

--- dist/types/components/groups/index.d.ts
+++ dist/types/components/groups/index.d.ts
@@ -1,3 +1,3 @@
 export { default as XcLayers } from './XcLayers';
 export { default as XcInteractions } from './XcInteractions';
-export { default as XcOverlays } from './XcOverlays.tsx';
+export { default as XcOverlays } from './XcOverlays';
