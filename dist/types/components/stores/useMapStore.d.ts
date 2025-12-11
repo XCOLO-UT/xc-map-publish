21ad6128a49fef5ef18fcdd634410b4c5fa86fc6
DKJung <zzliou33@xcolo.co.kr>
2025-12-11 11:44:51 +0900

    package.json 수정
     package.json 수정
    불필요 라이브러리 제거

--- dist/types/components/stores/useMapStore.d.ts
+++ dist/types/components/stores/useMapStore.d.ts
@@ -1,13 +1,2 @@
-import { Map as OlMap } from "ol";
-interface IMapSet {
-    id: string;
-    map: OlMap | null;
-}
-interface IMapState {
-    mapSet: IMapSet[];
-    populateMap: (mapGenerated: OlMap, id: string) => void;
-    getMap: (id: string) => OlMap | undefined;
-    removeMap: (id: string) => void;
-}
-declare const useMapStore: import("zustand").UseBoundStore<import("zustand").StoreApi<IMapState>>;
+declare const useMapStore: any;
 export default useMapStore;
